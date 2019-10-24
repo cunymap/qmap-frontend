e8b0fec280d250ac76460aa4011fbef0cf766ecd
popoway <31576265+popoway@users.noreply.github.com>
2019-10-24 18:16:45 -0400

    Bootstrap CSS
    Refactorize login page with working navbar and responsive design. Also separate css/js file from html.

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+/*function for enabling other selects when the required one is selected'*/
+function myFunction() {
+  document.getElementById("majors").disabled = false;
+  document.getElementById("terms").disabled = false;
+}
+/*function to show the result according to the selection*/
+function showResult() {
+  document.getElementById("result").style.display = "block";
+  var major = document.getElementById("majors");
+  var majorResult = major.options[major.selectedIndex].text;
+  var term = document.getElementById("terms");
+  var termResult = term.options[term.selectedIndex].text;
+  document.getElementById("majorFreshman").innerHTML = "Welcome to " + majorResult + " " + termResult;
+  document.getElementById("majorSophomore").innerHTML = "Welcome to " + majorResult + " " + termResult;
+  document.getElementById("majorJunior").innerHTML = "Welcome to " + majorResult + " " + termResult;
+  document.getElementById("majorSenior").innerHTML = "Welcome to " + majorResult + " " + termResult;
+}
