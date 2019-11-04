d02affc9f22f345096aada61995239c8452f44da
xiaoxwang <55675754+xiaoxwang@users.noreply.github.com>
2019-11-03 20:49:03 -0500

    Create admin.php

---
+++ admin.php
@@ -0,0 +1,63 @@
+<!-- test purpose. Make changes later-->
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/x-icon" href="https://www2.cuny.edu/favicon.ico">
+
+    <title>Admin - CUNY Degree Maps</title>
+<html>
+   
+   <head>
+      <title>Welcome </title>
+   </head>
+   
+   <body>
+   <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="#">
+        <img src="https://seeklogo.com/images/C/cuny-logo-E85957DB00-seeklogo.com.png" height="30" class="d-inline-block align-top" alt="CUNY">
+        Degree Maps
+      </a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarText">
+        <ul class="navbar-nav mr-auto">
+        </ul>
+        <span class="navbar-text">
+          <a href = "login.html">Log out</a>
+        </span>
+      </div>
+      </nav>
+      <br>
+      <p>Welcome to admin page</p> 
+   
+
+      <!--footer-->	
+    <footer>
+      <div class="container-fluid text-center">
+        <div class="row">
+          <div class="col-12 col-md-2">
+          </div>
+          <div class="col-12 col-md-8">
+            <span class="text-center">
+              &copy; 2019 The Degree Maps Authors.
+              <a href="https://github.com/cunymap" target="_blank">GitHub</a>
+            </span>
+          </div>
+          <div class="col-12 col-md-2">
+          </div>
+        </div>
+      </div>
+    </footer>
+   </body>
+   
+</html>
