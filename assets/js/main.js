7bfd2f32a9e740ba852d661a8fcf0a18f39017dc
xiaoxwang <55675754+xiaoxwang@users.noreply.github.com>
2019-11-21 10:25:26 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,26 @@
-/*function for enabling other selects when the required one is selected'*/
-function myFunction() {
-  document.getElementById("majors").disabled = false;
-  document.getElementById("terms").disabled = false;
-}
-/*function to show the result according to the selection*/
-function showResult() {
-  document.getElementById("result").style.display = "block";
-  var major = document.getElementById("majors");
-  var majorResult = major.options[major.selectedIndex].text;
-  var term = document.getElementById("terms");
-  var termResult = term.options[term.selectedIndex].text;
-  document.getElementById("majorFreshman").innerHTML = "Welcome to " + majorResult + " " + termResult;
-  document.getElementById("majorSophomore").innerHTML = "Welcome to " + majorResult + " " + termResult;
-  document.getElementById("majorJunior").innerHTML = "Welcome to " + majorResult + " " + termResult;
-  document.getElementById("majorSenior").innerHTML = "Welcome to " + majorResult + " " + termResult;
-}
+
+function showMajor() {
+         document.getElementById("major").style.display = "block";
+         document.getElementById("prompt1").style.display = "none";
+         document.getElementById("prompt2").style.display = "block";
+      }
+      function showTerm() {
+         document.getElementById("term").style.display = "block";
+         document.getElementById("prompt2").style.display = "none";
+         document.getElementById("prompt3").style.display = "block";
+      }
+      function showResult() {
+         document.getElementById("prompt3").style.display = "none";
+         document.getElementById("result").style.display = "block";
+         var major = document.getElementById("majors");
+         var majorResult = major.options[major.selectedIndex].text;
+         var term = document.getElementById("terms");
+         var termResult = term.options[term.selectedIndex].text;
+         document.getElementById("majorFreshman").innerHTML = "Welcome to " + majorResult + " " + termResult;
+         document.getElementById("majorSophomore").innerHTML = "Welcome to " + majorResult + " " + termResult;
+         document.getElementById("majorJunior").innerHTML = "Welcome to " + majorResult + " " + termResult;
+         document.getElementById("majorSenior").innerHTML = "Welcome to " + majorResult + " " + termResult;
+      }
 
 // initialize Bootstrap tooltip
 $( document ).ready(function() {
