fcf89a9d29cd1f3f95da6f54bb9442b921b2022c
Matthew Ross <31318280+Rozaybecrazy@users.noreply.github.com>
2019-12-05 23:31:47 -0500

    Files to delete Map Uploaded
    The code searches for maps and displays them in a semester table properly. I wrote a php file which is supposed to delete the map but was having trouble with it. I don't want to waste our time on it, as Im a bit slow with it. Whats left is to style it and connect it to the php file. Have at it team.

---
+++ deleteMap.js
@@ -0,0 +1,58 @@
+function askAgain()
+{
+  var btn = document.getElementById("delete_btn");
+  btn.value = "Confirm Deletion";
+}
+function validate()
+{
+    if (document.getElementById("map_id").value == "");
+    {
+        document.getElementById("output").innerHTML="Empty Input";
+        return false;
+    }
+    return true;
+}
+
+function loadMap()
+{
+  document.getElementById("delete_btn").hidden = true;
+  var idField = document.getElementById("map_id");
+  var APIIdURL = "https://cs355map.herokuapp.com/api/map/";
+  var id = idField.value;
+  APIIdURL += id + "/?format=json";
+
+   refreshTable();
+  var xmlhttp = new XMLHttpRequest();
+  console.log(xmlhttp);
+  xmlhttp.onreadystatechange = function() {
+    console.log("Loading degree list for " + id + "...");
+    if (this.readyState == 4 && this.status == 200) {
+      var idsObject = JSON.parse(this.responseText);
+      document.getElementById("output").innerHTML = "";
+      for (var i = 0; i < idsObject.length; i++) {
+        var tile = "sem" + idsObject[i].semester_num;
+        var text = document.createTextNode(idsObject[i].subject + " " + idsObject[i].catalog + " -- " + idsObject[i].descr);
+        var br = document.createElement("br");
+        console.log(tile);
+        document.getElementById(tile).appendChild(text);
+        document.getElementById(tile).appendChild(br);
+        document.getElementById("delete_btn").hidden = false;
+      }
+
+    }
+    else {
+      document.getElementById("output").innerHTML = "Path ID does not exists";
+    }
+}
+xmlhttp.open("GET", APIIdURL, true);
+xmlhttp.send();
+}
+
+function refreshTable()
+{
+  for(var i = 1; i<=8; i++)
+  {
+      document.getElementById("output").innerHTML = "";
+      document.getElementById("sem" + i).innerHTML = "";
+  }
+}
---
+++ deleteMap.php
@@ -0,0 +1,25 @@
+<?php
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "myDB";
+$id = $_POST['map_id'];
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// sql to delete a record
+$sql = "DELETE FROM map WHERE map_id=" . $id;
+
+if ($conn->query($sql) === TRUE) {
+    echo "Record deleted successfully";
+} else {
+    echo "Error deleting record: " . $conn->error;
+}
+
+$conn->close();
+?>
